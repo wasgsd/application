b46d15d07823f84e0bdfaaba0efc21897d99bc8b
Pavlo Vodopyan <pavel.vodopyan@gmail.com>
2016-12-01 17:16:02 +0000

    Add new template helper to accept format string.

--- app.js
+++ app.js
@@ -11,39 +11,11 @@ var app = express();
 
 // View engine setup
 var handlebars = require('express-handlebars')
-    .create({
-        defaultLayout : 'main',
-        extname       : '.hbs',
-        helpers : {
-            // Handlebars does not allow to have conditions in IF, here is
-            // workaround picked from here: http://stackoverflow.com/questions/8853396/logical-operator-in-a-handlebars-js-if-conditional
-            if_equal :  function(v1, v2, options){
-                if(v1 === v2) {
-                    return options.fn(this);
-                }
-                return options.inverse(this);
-            },
-
-            as_date : function(date_string, options) {
-
-              // By default we considering that we are in UTC time
-              var utc_offset = 0;
-
-              if ( options.hasOwnProperty('data')
-                && options.data.hasOwnProperty('root')
-                && options.data.root.hasOwnProperty('logged_user')
-                && options.data.root.logged_user
-                && options.data.root.logged_user.hasOwnProperty('company')
-              ) {
-                utc_offset = options.data.root.logged_user.company.get_utc_offset();
-              }
-
-              if (! date_string) return '';
-
-              return moment(date_string).utcOffset(utc_offset).format('YYYY-MM-DD');
-            },
-        }
-    });
+  .create({
+    defaultLayout : 'main',
+    extname       : '.hbs',
+    helpers       : require('./lib/view/helpers')(),
+  });
 
 app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
